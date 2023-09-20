d4d7c3ee3ed5f7b38dade875311011bb707cf986
Suresh Lasantha <lasanthasuresh@gmail.com>
2023-09-20 11:32:14 +0000

    adding the files

---
+++ PowerCalculator.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class PowerCalculator {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Enter the base number: ");
+    double base = scanner.nextDouble();
+
+    System.out.println("Enter the exponent: ");
+    double exponent = scanner.nextDouble();
+
+    double power = calculatePower(base, exponent);
+    System.out.println(f"The power of {base} raised to {exponent} is {power}");
+  }
+
+  private static double calculatePower(double base, double exponent) {
+    double result = 1;
+    for (int i = 0; i < exponent; i++) {
+      result *= base;
+    }
+    return result;
+  }
+}
---
+++ power_calculator.py
@@ -0,0 +1,29 @@
+def calculate_power(base, exponent):
+  """Calculates the power of a number.
+
+  Args:
+    base: The base number.
+    exponent: The exponent.
+
+  Returns:
+    The power of the base number raised to the exponent.
+  """
+
+  result = 1
+  for i in range(exponent):
+    result *= base
+  return result
+
+
+def main():
+  """Prompts the user for two numbers and prints the power of those numbers."""
+
+  base = float(input("Enter the base number: "))
+  exponent = float(input("Enter the exponent: "))
+
+  power = calculate_power(base, exponent)
+  print(f"The power of {base} raised to {exponent} is {power}")
+
+
+if __name__ == "__main__":
+  main()
